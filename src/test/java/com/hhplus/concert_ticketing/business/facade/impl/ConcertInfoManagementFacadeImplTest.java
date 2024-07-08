6ca832786aaedd828c7613db89beefde2072af97
[헬스케어]박시현A <si0852@douzone.com>
2024-07-08 15:43:54 +0900

    si0852 feat 콘서트 예약 가능한 날짜 조회 API 비지니스 로직 구현 및 단위테스트 진행

---
+++ src/test/java/com/hhplus/concert_ticketing/business/facade/impl/ConcertInfoManagementFacadeImplTest.java
@@ -0,0 +1,159 @@
+package com.hhplus.concert_ticketing.business.facade.impl;
+
+import com.hhplus.concert_ticketing.business.entity.Concert;
+import com.hhplus.concert_ticketing.business.entity.ConcertOption;
+import com.hhplus.concert_ticketing.business.entity.Token;
+import com.hhplus.concert_ticketing.business.service.impl.ConcertOptionServiceImpl;
+import com.hhplus.concert_ticketing.business.service.impl.ConcertServiceImpl;
+import com.hhplus.concert_ticketing.business.service.impl.TokenQueueServiceImpl;
+import com.hhplus.concert_ticketing.status.TokenStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class ConcertInfoManagementFacadeImplTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ConcertInfoManagementFacadeImplTest.class);
+
+    @InjectMocks
+    ConcertInfoManagementFacadeImpl concertInfoManagementFacade;
+
+    @Mock
+    TokenQueueServiceImpl tokenQueueService;
+
+    @Mock
+    ConcertOptionServiceImpl concertOptionService;
+
+    @Mock
+    ConcertServiceImpl concertService;
+
+    @DisplayName("토큰 유효성 체크, 토큰 상태가 ACTIVE일 경우")
+    @Test
+    void checking_token_and_stats_is_Active() {
+        //given
+        Long userId = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        Token token = new Token(userId, "token123123", TokenStatus.ACTIVE.toString(), now, now.plusMinutes(10));
+        when(tokenQueueService.validateToken(userId)).thenReturn(token);
+
+        // when
+        Long concertId = 1L;
+        // then
+        assertThrows(RuntimeException.class, () -> {
+            concertInfoManagementFacade.getConcertOption(userId, concertId);
+        });
+    }
+
+    @DisplayName("토큰 유효성 체크, 토큰 상태가 WAITING일 경우")
+    @Test
+    void checking_token_and_stats_is_Waiting() {
+        //given
+        Long userId = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        Token token = new Token(userId, "token123123", TokenStatus.WAITING.toString(), now, now.plusMinutes(10));
+        when(tokenQueueService.validateToken(userId)).thenReturn(token);
+
+        // when
+        Long concertId = 1L;
+        // then
+        assertThrows(RuntimeException.class, () -> {
+            concertInfoManagementFacade.getConcertOption(userId, concertId);
+        });
+    }
+
+    @DisplayName("콘서트 유효성 체크, Concert가 null일 경우")
+    @Test
+    void checking_concert_is_null() {
+        //given
+        Long userId = 1L;
+        Long concertId = 1L;
+        when(tokenQueueService.validateToken(userId)).thenReturn(null);
+        when(concertService.getConcertData(concertId)).thenReturn(null);
+
+        //when && then
+        assertThrows(RuntimeException.class, () -> {
+            concertInfoManagementFacade.getConcertOption(userId, concertId);
+        });
+    }
+
+    @DisplayName("콘서트 유효성 체크, Concert가 null일 경우 (토큰 상태가 만료일 경우)")
+    @Test
+    void checking_concert_is_null2() {
+        //given
+        Long userId = 1L;
+        Long concertId = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        Token token = new Token(userId, "token123123", TokenStatus.EXPIRED.toString(), now, now.plusMinutes(10));
+        when(tokenQueueService.validateToken(userId)).thenReturn(token);
+        when(concertService.getConcertData(concertId)).thenReturn(null);
+
+        //when && then
+        assertThrows(RuntimeException.class, () -> {
+            concertInfoManagementFacade.getConcertOption(userId, concertId);
+        });
+    }
+
+    @DisplayName("콘서트 옵션 체크, ConcertOption 리스트 크기가 0일 경우")
+    @Test
+    void checking_concertOption_list_size_0() {
+        //given
+        Long userId = 1L;
+        Long concertId = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        Token token = new Token(userId, "token123123", TokenStatus.EXPIRED.toString(), now, now.plusMinutes(10));
+        when(tokenQueueService.validateToken(userId)).thenReturn(token);
+        Concert concert = new Concert("콘서트");
+        when(concertService.getConcertData(concertId)).thenReturn(concert);
+
+        //when
+        List<ConcertOption> concertOptionList = List.of();
+        when(concertOptionService.getConcertOptionData(concertId)).thenReturn(concertOptionList);
+
+        //then
+        assertThrows(RuntimeException.class, () -> {
+            concertInfoManagementFacade.getConcertOption(userId, concertId);
+        });
+    }
+
+    @DisplayName("콘서트 옵션 체크, ConcertOption 리스트 크기가 0보다 클 경우")
+    @Test
+    void checking_concertOption_list_size_1() {
+        //given
+        Long userId = 1L;
+        Long concertId = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        Token token = new Token(userId, "token123123", TokenStatus.EXPIRED.toString(), now, now.plusMinutes(10));
+        when(tokenQueueService.validateToken(userId)).thenReturn(token);
+        Concert concert = new Concert("콘서트");
+        when(concertService.getConcertData(concertId)).thenReturn(concert);
+
+        //when
+        List<ConcertOption> concertOptionList = List.of(
+                new ConcertOption(1L, now, 10000.0),
+                new ConcertOption(1L, now.minusHours(3), 14000.0),
+                new ConcertOption(1L, now.plusHours(3), 12000.0)
+        );
+
+        when(concertOptionService.getConcertOptionData(concertId)).thenReturn(concertOptionList);
+        List<ConcertOption> concertOption = concertInfoManagementFacade.getConcertOption(userId, concertId);
+        //then
+        assertThat(concertOption).isEqualTo(concertOptionList);
+    }
+}
